fac7a6debcd98f72d684d3615f50e6311e11b4f5
Joshua Rose <45501010+CodingLife0@users.noreply.github.com>
2019-08-28 16:38:02 +1000

    Create members.py

---
+++ cogs/members.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+
+
+class Members(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.guild_only()
+    async def joined(self, ctx, *, member: discord.Member):
+        """Says when a member joined."""
+        await ctx.send(f'{member.display_name} joined on {member.joined_at}')
+
+    @commands.command(name='coolbot')
+    async def cool_bot(self, ctx):
+        """Is the bot cool?"""
+        await ctx.send('This bot is cool. :)')
+
+    @commands.command(name='top_role', aliases=['toprole'])
+    @commands.guild_only()
+    async def show_toprole(self, ctx, *, member: discord.Member=None):
+        """Simple command which shows the members Top Role."""
+
+        if member is None:
+            member = ctx.author
+        embed = discord.Embed(title='Overview',
+                              description='',
+                              colour=0x98FB98)
+        
+        embed.set_author(name='Ghento Bot Information',
+                         url='https://discordapp.com/api/oauth2/authorize?client_id=616092750051409928&permissions=8&scope=bot',
+                         icon_url='https://cdn.discordapp.com/attachments/615011904410222595/616109163315200020/istockphoto-673647420-612x612.jpg')
+
+
+        await ctx.send(f'The top role for {member.display_name} is {member.top_role.name}')
+    
+    @commands.command(name='perms', aliases=['perms_for', 'permissions'])
+    @commands.guild_only()
+    async def check_permissions(self, ctx, *, member: discord.Member=None):
+        """A simple command which checks a members Guild Permissions.
+        If member is not provided, the author will be checked."""
+
+        if not member:
+            member = ctx.author
+
+        # Here we check if the value of each permission is True.
+        perms = '\n'.join(perm for perm, value in member.guild_permissions if value)
+
+        # And to make it look nice, we wrap it in an Embed.
+        embed = discord.Embed(title='Server Permissions:', description=ctx.guild.name, colour=member.colour)
+        embed.set_author(icon_url=member.avatar_url, name=str(member))
+
+        # \uFEFF is a Zero-Width Space, which basically allows us to have an empty field name.
+        embed.add_field(name='\uFEFF', value=perms)
+
+        await ctx.send(content=None, embed=embed)
+        # Thanks to Gio for the Command.
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(Members(bot))
